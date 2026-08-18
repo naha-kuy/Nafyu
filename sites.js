3429574448706bb422b63acd23defdf34453dbda
Nafis Hakim <muhammadnafishakim@gmail.com>
2026-08-18 07:18:26 +0700

    url

--- sites.js
+++ sites.js
@@ -2,7 +2,7 @@ const TIPES = [
   { id: "1", label: "Semua" },
   { id: "2", label: "Jadwal" },
   { id: "3", label: "Kerjaan" },
-  { id: "4", label: "Produktivitas" },
+  { id: "4", label: "Sheet" },
   { id: "5", label: "Lainnya" },
 ];
 
@@ -22,5 +22,18 @@ const SITES = [
   { nama: "Fee Mathematalk",
     url: "https://docs.google.com/spreadsheets/d/1LjId8Gz9HWswfwniDhr9P1XSepOVmCxZjkOPTKjWA-k/edit?usp=sharing",
     tipe: ["3", "4"],
-    tag: "Finansial Analysis Fee" }
+    tag: "Finansial Analysis Fee" },
+  { nama: "Monthly Finance",
+    url: "https://docs.google.com/spreadsheets/d/1WxE72fX_lnB6hgNdf7zYk8Dpm244bVE8IV2WlE7K-pA/edit?usp=sharing",
+    tipe: ["3", "4"],
+    tag: "Finansial Monthly Analysis" },
+  { nama: "Tanggal Kembar",
+    url: "https://docs.google.com/spreadsheets/d/1gXP7xTAPCSDxNLhqM7aUIOTIZyQ8Ul4s9jQ8XWx0LXw/edit?usp=drive_link",
+    tipe: "4",
+    tag: "Budgeting Promo" },
+  { nama: "Password",
+    url: "https://docs.google.com/spreadsheets/d/16C6vWxeJoG_MYONgFVNkGyWTUu8ntGc3PqYUq6NNMhY/edit?usp=sharing",
+    tipe: "3",
+    tag: "Password Account NafYu" }
+
 ];
